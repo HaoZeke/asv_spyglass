317bd89419dedd64d2118b6cd6ce448bd89e7b95
Rohit Goswami <rog32@hi.is>
2025-01-19 22:23:51 +0000

    ENH: Rework benchmarks to be closer to ASV

--- src/asv_spyglass/_asv_ro.py
+++ src/asv_spyglass/_asv_ro.py
@@ -1,5 +1,5 @@
+import itertools
 import json
-import pprint as pp
 import re
 from pathlib import Path
 from typing import Iterator, Union
@@ -24,51 +24,54 @@ class ReadOnlyASVBenchmarks:
                 Defaults to None (all benchmarks included).
         """
         d = asv_json_load(getstrform(benchmarks_file), api_version=self.api_version)
-        self._benchmarks = d.values()
-        self._filtered_benchmarks = self._filter_benchmarks(regex)
+        self._all_benchmarks = {}  # Store all benchmarks here
+        self._benchmark_selection = {}  # Track selected parameter combinations
+        self.filtered_benchmarks = {}  # Store selected benchmarks here
+
+        if not regex:
+            regex = []
+        if isinstance(regex, str):
+            regex = [regex]
+
+        for benchmark in d.values():
+            self._all_benchmarks[benchmark["name"]] = benchmark
+            if benchmark["params"]:
+                self._benchmark_selection[benchmark["name"]] = []
+                for idx, param_set in enumerate(
+                    itertools.product(*benchmark["params"])
+                ):
+                    name = f"{benchmark['name']}({', '.join(param_set)})"
+                    if not regex or any(re.search(reg, name) for reg in regex):
+                        self.filtered_benchmarks[name] = (
+                            benchmark  # Store with full name
+                        )
+                        self._benchmark_selection[benchmark["name"]].append(idx)
+            else:
+                self._benchmark_selection[benchmark["name"]] = None
+                if not regex or any(re.search(reg, benchmark["name"]) for reg in regex):
+                    self.filtered_benchmarks[benchmark["name"]] = benchmark
 
     def __iter__(self) -> Iterator[dict]:
         """Iterate over the filtered benchmarks."""
-        return iter(self._filtered_benchmarks.values())
+        return iter(self.filtered_benchmarks.values())
 
     def __getitem__(self, name: str) -> Union[dict, None]:
         """Get a benchmark by name."""
-        return self._filtered_benchmarks.get(name)
+        return self.filtered_benchmarks.get(name)
 
     def __repr__(self) -> str:
-        return pp.pformat(self._filtered_benchmarks)
+        """Return a string representation of the filtered benchmarks."""
+        import pprint
+
+        pp = pprint.PrettyPrinter()
+        return pp.pformat(self.filtered_benchmarks)
 
     @property
     def benchmark_names(self) -> list[str]:
         """Get a list of benchmark names."""
-        return list(self._filtered_benchmarks.keys())
+        return list(self.filtered_benchmarks.keys())
 
     @property
-    def benchmarks(self) -> list[str]:
-        """Get a list of benchmark names."""
-        return self._filtered_benchmarks
-
-    def _filter_benchmarks(
-        self, regex: Union[str, list[str]] = None
-    ) -> dict[str, dict]:
-        """
-        Filter benchmarks based on provided regular expression(s).
-
-        Args:
-            regex (Union[str, list[str]], optional): Regular expression(s) to filter benchmarks.
-                Defaults to None (all benchmarks included).
-
-        Returns:
-            dict[str, dict]: A dictionary containing filtered benchmarks.
-        """
-        if not regex:
-            return {benchmark["name"]: benchmark for benchmark in self._benchmarks}
-
-        if isinstance(regex, str):
-            regex = [regex]
-
-        filtered_benchmarks = {}
-        for benchmark in self._benchmarks:
-            if any(re.search(r, benchmark["name"]) for r in regex):
-                filtered_benchmarks[benchmark["name"]] = benchmark
-        return filtered_benchmarks
+    def benchmarks(self) -> dict[str, dict]:
+        """Get a dictionary of filtered benchmarks."""
+        return self.filtered_benchmarks
