d4b49c91c33fcd0ebe11e34e7b1f09cfa308122d
Rohit Goswami <rog32@hi.is>
2025-01-20 03:07:10 +0000

    ENH: Add a single csv generator cli

--- src/asv_spyglass/cli.py
+++ src/asv_spyglass/cli.py
@@ -1,9 +1,22 @@
+from pathlib import Path
+
 import click
+from asv import results
 
-from asv_spyglass.compare import do_compare
+from asv_spyglass._asv_ro import ReadOnlyASVBenchmarks
+from asv_spyglass._aux import getstrform
+from asv_spyglass.compare import ResultPreparer, do_compare
 
 
-@click.command()
+@click.group()
+def cli():
+    """
+    Command-line interface for ASV benchmark analysis.
+    """
+    pass
+
+
+@cli.command()
 @click.argument("b1", type=click.Path(exists=True), required=True)
 @click.argument("b2", type=click.Path(exists=True), required=True)
 @click.argument("bconf", type=click.Path(exists=True), required=True)
@@ -30,8 +43,30 @@ from asv_spyglass.compare import do_compare
     show_default=True,
     help="Sort output by change, ratio, or name.",
 )
-def cli(b1, b2, bconf, factor, split, only_changed, sort):
+def compare(b1, b2, bconf, factor, split, only_changed, sort):  # Renamed to 'compare'
     """
     Compare two ASV result files.
     """
     print(do_compare(b1, b2, bconf, factor, split, only_changed, sort))
+
+
+@cli.command()
+@click.argument("bres", type=click.Path(exists=True), required=True)
+@click.argument("bdat", type=click.Path(exists=True), required=True)
+@click.option(
+    "--csv",
+    type=click.Path(),
+    help="Save data to csv",
+)
+def to_df(bres, bdat, csv):
+    """
+    Generate a dataframe from an ASV result file.
+    """
+    res = results.Results.load(bres)
+    benchdat = ReadOnlyASVBenchmarks(Path(bdat)).benchmarks
+    preparer = ResultPreparer(benchdat)
+    df = preparer.prepare(res).to_df()
+    if csv:
+        df.to_csv(csv)
+    else:
+        click.echo(df)
